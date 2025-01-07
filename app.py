18db808e4824d122dc8d8ebc0ae253045dbf6de8
Marcus Lin <marcuslin@Marcuss-MacBook-Pro.local>
2025-01-06 21:55:52 -0800

    successfully fetch temperature from api

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 from flask import Flask, render_template
+import requests
 import config
 
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+def get_weather(city):
+    try:
+        response = requests.get(config.URL)
+        response.raise_for_status()
+        data = response.json()
+        return {
+            "city": data["name"],
+            "temperature": data["main"]["temp"],
+            "description": data["weather"][0]["description"]
+        }
+    except Exception as e:
+        print(f"Error fetching weather: {e}")
+    except KeyError as e:
+        print(f"Data format error: Missing key {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
+
+    return None
+
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    city = config.DEFAULT_CITY
+    weather_data = get_weather(city)
+
+    return render_template("index.html", weather=weather_data)
 
 
 if __name__ == "__main__":
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 API_KEY = "552469047f32b0fc8a568364312befc2"
-DEFAULT_CITY = "Berkeley"
-UNITS = "Imperial"
+DEFAULT_CITY = "Berkeley, CA"
+lat = "37.8715"
+URL = f"https://api.openweathermap.org/data/2.5/weather?q=Berkeley,CA&appid=552469047f32b0fc8a568364312befc2&units=metric"
